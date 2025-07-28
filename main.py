fb5b4dc21c15e2ad0d720109d00cb425d0330a80
Daniel Ezeh <daniel.ezeh@nomba.com>
2025-07-28 21:21:37 +0100

    Updated.

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 #This is on master
 
 print("Hello world!")
-=======
 # This is a simple python file.
 a = 5
 b =7
 c = a + b
 
 print(f"The sum of {a} and {b} is {c}")
->>>>>>> my-branch-1
